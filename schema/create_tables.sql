4d687a0a25902a0e4bbb7123ce7c6d2430bcdf1a
Bolbol <134863700+BelboulLeFurieux@users.noreply.github.com>
2026-01-11 15:43:22 +0100

    Update create_tables.sql
    finished the creation of the tables

--- schema/create_tables.sql
+++ schema/create_tables.sql
@@ -1,14 +1,33 @@
-CREATE DATABASE country_analysis
+CREATE DATABASE country_analysis;
+
+USE country_analysis;
 
 CREATE TABLE continent (
-continentId INT,
-name VARCHAR(20),
-  );
+    continentId INT PRIMARY KEY,
+    name VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE country (
+    countryId INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    continentId INT,
+    superficie BIGINT,
+    codeIso CHAR(2) UNIQUE,
+    FOREIGN KEY (continentId) REFERENCES continent(continentId)
+);
 
 
+CREATE TABLE metrics (
+  metricId INT PRIMARY KEY,
+  name VARCHAR(20) NOT NULL
+);
 
-CREATE TABLE country
-
-CREATE TABLE index
-
-CREATE TABLE country_index
+CREATE TABLE country_metrics (
+  metricId INT,
+  countryId INT,
+  metricYear YEAR,
+  metricValue DECIMAL(15,2),
+  PRIMARY KEY(metricId, countryId, metricYear),
+  FOREIGN KEY (metricId) REFERENCES metrics(metricId),
+  FOREIGN KEY (countryId) REFERENCES country(countryId)
+);
